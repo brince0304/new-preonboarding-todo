fdaa0257423919d4cebc9621d6a0c6ddc4e334c7
brince0304 <brince0304@gmail.com>
2023-07-05 15:16:14 +0900

    style: Input 컴포넌트 코드 포멧팅

--- src/components/common/Input.style.ts
+++ src/components/common/Input.style.ts
@@ -1,55 +1,55 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    position: relative;
-    gap: 5px;
-    label {
-        font-size: 16px;
-        font-weight: bold;
-    }
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: relative;
+  gap: 5px;
+  label {
+    font-size: 16px;
+    font-weight: bold;
+  }
 `;
 
 export const Input = styled.input<StyledInputProps>`
-    width: ${({ width }) => width || '100%'};
-    height: ${({ height }) => height || '40px'};
-    padding: 5px 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    font-size: ${({ fontSize }) => fontSize || '16px'};
-    font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
-    color: ${({ color }) => color || '#121212'};
-    box-sizing: border-box;
-    outline: none;
-    &:focus {
-        border: 1px solid #000;
-    }
+  width: ${({ width }) => width || '100%'};
+  height: ${({ height }) => height || '40px'};
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: ${({ fontSize }) => fontSize || '16px'};
+  font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
+  color: ${({ color }) => color || '#121212'};
+  box-sizing: border-box;
+  outline: none;
+  &:focus {
+    border: 1px solid #000;
+  }
 `;
 
 export const IconWrap = styled.div`
-position: absolute;
-right: 10px;
-top: 50%;
+  position: absolute;
+  right: 10px;
+  top: 50%;
 `;
 
 export const HelperText = styled.span<StyledHelpertextProps>`
-color: ${({ error }) => error ? 'red' : '#000'};
-font-size: 12px;
-font-weight: ${({ error }) => error ? '500' : 'normal'};
+  color: ${({ error }) => (error ? 'red' : '#000')};
+  font-size: 12px;
+  font-weight: ${({ error }) => (error ? '500' : 'normal')};
 `;
 
 interface StyledInputProps {
-    width?: string;
-    height?: string;
-    fontSize?: string;
-    fontWeight?: string;
-    color?: string;
+  width?: string;
+  height?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  color?: string;
 }
 
 interface StyledHelpertextProps {
-    error?: boolean;
+  error?: boolean;
 }
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,33 +1,26 @@
-import { ForwardedRef, forwardRef, HTMLProps, ReactNode } from "react";
-import * as S from "./Input.style";
+import { ForwardedRef, forwardRef, HTMLProps, ReactNode } from 'react';
+import * as S from './Input.style';
 
-const Input = forwardRef((props:IInputProps,ref:ForwardedRef<HTMLInputElement>) => {
-    const {
-        error,
-        helperText,
-        icon,
-        ...rest
-    } = props;
-    return (
-        <S.Container>
-    <S.Input  ref={ref} {...rest} />
-    <S.IconWrap>
-        {icon}
-    </S.IconWrap>
-    <S.HelperText error={error}>{helperText}</S.HelperText>
+const Input = forwardRef((props: IInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+  const { error, helperText, icon, ...rest } = props;
+  return (
+    <S.Container>
+      <S.Input ref={ref} {...rest} />
+      <S.IconWrap>{icon}</S.IconWrap>
+      <S.HelperText error={error}>{helperText}</S.HelperText>
     </S.Container>
-    );
+  );
 });
 
-interface IInputProps extends Omit<HTMLProps<HTMLInputElement>, "ref"> {
-    error?: boolean;
-    helperText?: string;
-    width?: string;
-    height?: string;
-    color?: string;
-    fontSize?: string;
-    fontWeight?: string;
-    icon ?: ReactNode;
+interface IInputProps extends Omit<HTMLProps<HTMLInputElement>, 'ref'> {
+  error?: boolean;
+  helperText?: string;
+  width?: string;
+  height?: string;
+  color?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  icon?: ReactNode;
 }
 
 export default Input;
