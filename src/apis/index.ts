74c9601b062a299378cd668a077a56a579f8ea7d
brince0304 <brince0304@gmail.com>
2023-07-05 14:09:17 +0900

    style: axios 코드 포멧팅

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,38 +1,36 @@
 import axios, { AxiosInstance } from 'axios';
 
-
-const axiosInstance = (url: string, options:any) => {
-    return axios.create({
-        baseURL: url,
-        ...options
-    })
-}
+const axiosInstance = (url: string, options: any) => {
+  return axios.create({
+    baseURL: url,
+    ...options,
+  });
+};
 
 const setInterceptors = (instance: AxiosInstance) => {
-    instance.interceptors.request.use(
-        function (config) {
-            const token = localStorage.getItem('accessToken');
-            config.headers.Authorization = token ? `Bearer ${token}` : '';
-            return config;
-        },
-        function (error) {
-            return Promise.reject(error);
-        }
-    );
+  instance.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem('accessToken');
+      config.headers.Authorization = token ? `Bearer ${token}` : '';
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    },
+  );
 
-    instance.interceptors.response.use(
-        function (response) {
-        
-            return response;
-        },
+  instance.interceptors.response.use(
+    function (response) {
+      return response;
+    },
 
-        function (error) {
-            if(error.response.status === 401) {
-                localStorage.removeItem('accessToken');
-            return Promise.reject(error);
-        }
-    }
-    );
+    function (error) {
+      if (error.response.status === 401) {
+        localStorage.removeItem('accessToken');
+        return Promise.reject(error);
+      }
+    },
+  );
 };
 
 const baseURL = 'https://www.pre-onboarding-selection-task.shop/';
