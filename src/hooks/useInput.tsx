dfd23f3238569b76325571d4031e749e19d706b0
brince0304 <brince0304@gmail.com>
2023-07-05 14:38:38 +0900

    style: useInput 코드 포멧팅

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,37 +1,36 @@
-import { ChangeEvent, MutableRefObject, useState } from "react";
+import { ChangeEvent, MutableRefObject, useState } from 'react';
 
-interface UseInputProps <T> {
-    initialValue: T;
-    regex?: RegExp;
-    refObject? : MutableRefObject<HTMLInputElement>;
+interface UseInputProps<T> {
+  initialValue: T;
+  regex?: RegExp;
+  refObject?: MutableRefObject<HTMLInputElement>;
 }
 
-const useInput = <T,>({initialValue, regex, refObject}: UseInputProps<T>) => {
-    const [value, setValue] = useState<T>(initialValue);
-    const isValidate = regex && typeof value === "string" ? regex.test(value) : true;
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value as unknown as T);
+const useInput = <T,>({ initialValue, regex, refObject }: UseInputProps<T>) => {
+  const [value, setValue] = useState<T>(initialValue);
+  const isValidate = regex && typeof value === 'string' ? regex.test(value) : true;
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value as unknown as T);
+  };
+  const setFocus = () => {
+    if (refObject && refObject.current) {
+      refObject.current.focus();
     }
-    const setFocus = () => {
-        if (refObject && refObject.current) {
-            refObject.current.focus();
-        }
+  };
+  const setBlur = () => {
+    if (refObject && refObject.current) {
+      refObject.current.blur();
     }
-    const setBlur = () => {
-        if (refObject && refObject.current) {
-            refObject.current.blur();
-        }
-    }
+  };
 
-    return {
-        value,
-        setValue,
-        onChange,
-        isValidate,
-        setFocus,
-        setBlur,
-    }
-    
-}
+  return {
+    value,
+    setValue,
+    onChange,
+    isValidate,
+    setFocus,
+    setBlur,
+  };
+};
 
 export default useInput;
