30865da4597e52d0dc93db9c72daa000f92cce28
brince0304 <brince0304@gmail.com>
2023-07-06 18:44:02 +0900

    style: Todo api 코드 포멧팅

--- src/apis/todo/index.ts
+++ src/apis/todo/index.ts
@@ -1,24 +1,24 @@
-import client from "apis/client";
+import client from 'apis/client';
 
-export const createTodo = async (content: string) : Promise<ITodo> => {
-return await client.post('/todos', {content});
-}
+export const createTodo = async (content: string): Promise<ITodo> => {
+  return await client.post('/todos', { content });
+};
 
-export const getTodos = async () : Promise<ITodo[]> => {
-    return await client.get('/todos');
-}
+export const getTodos = async (): Promise<ITodo[]> => {
+  return await client.get('/todos');
+};
 
-export const updateTodo = async (data:ITodo) : Promise<ITodo> => {
-    return await client.put(`/todos/${data.id}`, data);
-}
+export const updateTodo = async (data: ITodo): Promise<ITodo> => {
+  return await client.put(`/todos/${data.id}`, data);
+};
 
-export const deleteTodo = async (id:number) : Promise<ITodo> => {
-    return await client.delete(`/todos/${id}`);
-}
+export const deleteTodo = async (id: number): Promise<ITodo> => {
+  return await client.delete(`/todos/${id}`);
+};
 
-export interface ITodo{
-    id : number;
-    todo : string;
-    isCompleted : boolean;
-    userId : number;
+export interface ITodo {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: number;
 }
