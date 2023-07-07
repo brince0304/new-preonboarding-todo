4cb6d788601c223420fcdc0903438e484c487041
brince0304 <brince0304@gmail.com>
2023-07-07 14:22:43 +0900

    style: Header 코드 포멧팅

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import { HappyCatIcon } from 'components/common/Icon';
 import { useAuthState } from 'context';
 import { useNavigate } from 'react-router-dom';
 import * as S from './Header.style';
-import {  MouseEvent } from 'react';
+import { MouseEvent } from 'react';
 import useToast, { IUseToastProps } from 'hooks/useToast';
 
 const Header = () => {
@@ -11,7 +11,7 @@ const Header = () => {
     autoHideDuration: 2000,
     sx: { width: '100%' },
     children: '로그아웃에 성공했습니다. 2초 후에 메인 페이지로 이동합니다.',
-  } as IUseToastProps
+  } as IUseToastProps;
   const { toast: SignOutToast, handleOpenToast: handleOpenSignOutToast } = useToast(toastProps);
   const authState = useAuthState();
   const navigate = useNavigate();
@@ -26,20 +26,19 @@ const Header = () => {
     } else if (name === '로그인') {
       navigate('/signin');
     } else if (name === '회원가입') {
-    
       navigate('/signup');
     }
   };
 
-    const handleClickMenu = (e: MouseEvent<HTMLLIElement>) => {
-        const name = e.currentTarget.innerHTML;
-      handleNavigate(name);
-    };
+  const handleClickMenu = (e: MouseEvent<HTMLLIElement>) => {
+    const name = e.currentTarget.innerHTML;
+    handleNavigate(name);
+  };
 
   return (
     <S.Header>
       <S.Logo>
-        <HappyCatIcon width='70px' height='70px' />
+        <HappyCatIcon width="70px" height="70px" />
       </S.Logo>
       <S.Menus>
         <S.Menu>
