1db90da3d452d2f040cdc7957a15a5b96c24e07f
brince0304 <brince0304@gmail.com>
2023-07-11 17:49:44 +0900

    feat: api관리를 위한 context 생성

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { AuthServiceInterface, IAuth } from 'services/authService';
+
+const authContext = createContext<IAuthContextReturn | null>(null);
+export const useAuth = () => useContext(authContext) as IAuthContextReturn;
+
+// eslint-disable-next-line no-undef
+export const AuthProvider = ({
+  children,
+  authService,
+}: {
+  children: React.ReactNode;
+  authService: AuthServiceInterface;
+}) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const signin = authService.signin.bind(authService);
+  const signup = authService.signup.bind(authService);
+  const signout = authService.signout.bind(authService);
+  const signinCallback = async (data: IAuth) => {
+    await signin(data);
+    setIsAuthenticated(true);
+  };
+  const signupCallback = async (data: IAuth) => {
+    await signup(data);
+  };
+  const signoutCallback = async () => {
+    signout();
+    setIsAuthenticated(false);
+  };
+  useEffect(() => {
+    localStorage.getItem('token') ? setIsAuthenticated(true) : setIsAuthenticated(false);
+  }, [localStorage.getItem('token')]);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
+  return (
+    <authContext.Provider
+      value={{ isAuthenticated, signin: signinCallback, signup: signupCallback, signout: signoutCallback }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
+
+interface IAuthContextReturn {
+  isAuthenticated: boolean;
+  signin: (data: IAuth) => Promise<void>;
+  signup: (data: IAuth) => Promise<void>;
+  signout: () => Promise<void>;
+}
